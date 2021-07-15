0ae2a46a197031d150309ee3b9a60f7ab048e843
rdelange <ronald_de_lange@hotmail.com>
2021-07-15 22:17:41 +0200

    Fixed CouchDB bug where CouchDB initially gets overwritten with empty data

--- src/components/storage/pouchdb.svelte
+++ src/components/storage/pouchdb.svelte
@@ -39,6 +39,7 @@
         state.form.password = state.remote.password;
         state.form.database = state.remote.database;
         state.canSync = true;
+        
       }
     },
     isValidURL() {
@@ -57,14 +58,13 @@
         syncEnabled: true,
       });
       state.canSync = true;
-      console.log(remote);
-      pouchEngine.saveRemote(remote);
-      
+      state.remote = remote;
+      pouchEngine.saveRemote(remote); 
     },
     startSync() {
       state.syncing = true;
       state.remote.syncEnabled = true;
-      //pouchEngine.saveRemote(state.remote);
+      pouchEngine.saveRemote(state.remote);
       pouchEngine.startSync();
     },
     stopSync() {
