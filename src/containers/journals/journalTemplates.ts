621e7a57b7acbf5ec836b60382c43278763dc9e1
Ronald De Lange <ronalddelange@Ronalds-MacBook-Air.local>
2021-11-30 22:21:03 +0100

    guided journals updated

--- src/containers/journals/journalTemplates.ts
+++ src/containers/journals/journalTemplates.ts
@@ -1,7 +1,7 @@
 import type { IJournal } from "../../modules/journal/journal";
 
 export const journalTemplates: Array<IJournal> = [
-  {journalname: "Morning Journal Template",
+  {journalname: "Morning",
 displayName: "Morning",
 avatar:"",
 last: new Date(),
@@ -16,5 +16,13 @@ last: new Date(),
 notes: "This is a test for evening",
 prompts: ["How do you feel","Tell me about the day"],
 journalcolor: "#66BB6A",
+},
+{journalname: "Boilerplate",
+displayName: "Boilerplate",
+avatar:"",
+last: new Date(),
+notes: "This is a boilerplate for creating a guided journal",
+prompts: ["Placeholder for first prompt","Placeholde for second prompt"],
+journalcolor: "#66EE6A",
 }
 ]
